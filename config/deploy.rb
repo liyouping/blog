c8bdf55636392f8e5709aa6eefa917e8e713852a
lyp <453974610@qq.com>
2017-03-14 21:07:44 +0800

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -105,7 +105,7 @@ task :deploy => :environment do
     to :launch do
       queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
       #queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
-      queue "pumactl -F  #{deploy_to}/#{current_path}/config/puma.rb  start"
+      queue "pumactl -F  #{deploy_to}/#{current_path}/config/puma.rb  restart"
       invoke :'deploy:cleanup'
     end
   end
