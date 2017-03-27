bd3d7f40d9114ab84d9348487c15fcfec6edd275
MAC <MAC@MACdeiMac-2.local>
2017-03-27 10:17:28 +0800

    update config application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,28 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+	config.eager_load_paths += %W(#{Rails.root.join}/lib #{Rails.root.join('app', 'api', '*')})
+
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # Bower asset paths
+    root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
+      config.sass.load_paths << bower_path
+      config.assets.paths << bower_path
+    end
+	
+    # Precompile Bootstrap fonts
+    config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
+    # Minimum Sass number precision required by bootstrap-sass
+    ::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
+
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+	config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    
+    
   end
 end
